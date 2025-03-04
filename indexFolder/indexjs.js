52e62bf5bd08cd8a7eab5065af82ad5ab3851c85
Lukibear <lukastyon@gmail.com>
2025-03-04 11:14:03 -0600

    Added match day two and associated items

--- indexFolder/indexjs.js
+++ indexFolder/indexjs.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
-    $("#dayOneExpansion").click(function() {
-      $("#expandableContentDayOne").fadeToggle(1000);
-      
-    });
+  $("td[id$='Expansion']").click(function() {
+      $(this).closest("table").find("tbody[id^='expandableContent']").fadeToggle(1000);
+  });
+  $("#page-overlay").fadeOut(1500);
+
+  // 2️⃣ Function to fade in elements as they appear in the viewport
+  function fadeInOnScroll() {
+      $(".fade-in").each(function () {
+          var elementTop = $(this).offset().top;
+          var windowBottom = $(window).scrollTop() + $(window).height();
+
+          if (elementTop < windowBottom - 50) {
+              $(this).css("opacity", 1);
+          }
+      });
+  }
+
+  // Run fade-in on page load
+  fadeInOnScroll();
+
+  // Run fade-in on scroll
+  $(window).on("scroll", function () {
+      fadeInOnScroll();
+  });
+  
 });
